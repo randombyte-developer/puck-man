e15c1d0b94d79835e487ec1f5aa8f39bb0880bad
randombyte-developer <apps.randombyte@gmail.com>
2016-04-23 19:40:07 +0200

    Changed Start.java screen

--- Start.java
+++ Start.java
@@ -14,10 +14,14 @@ public class Start extends World {
         hintergrund.fill();
         setBackground(hintergrund);
         
-        addObject(new TextButton("Start", 70, Color.WHITE, Color.YELLOW, new TextButton.KlickCallback() {
+        addObject(new Text("PuckMan", 90, Color.GREEN, Color.LIGHT_GRAY), 2, 0);
+        
+        addObject(new TextButton("Start", 70, Color.LIGHT_GRAY, Color.YELLOW, new TextButton.KlickCallback() {
             public void klick() {
                 Greenfoot.setWorld(new LevelAuswahl());
             }
         }), 2, 1);
+        
+        addObject(new Text("von Sven Rahn und Kasper Jarocki", 18, Color.BLACK, Color.LIGHT_GRAY), 2, 2);
     }
 }
